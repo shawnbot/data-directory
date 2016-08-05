ef492a2c3d018d580da79e78c7d3d7ecdce39c30
Shawn Allen <shawn.allen@gsa.gov>
2016-08-05 14:00:59 -0700

    add caching

--- proxy.js
+++ proxy.js
@@ -4,33 +4,46 @@ var glob = require('glob');
 var formats = require('./formats');
 var formatExtensions = Object.keys(formats).join(',');
 
-module.exports = function createProxy(dir) {
+module.exports = function createProxy(dir, data) {
 
-  var data = Object.create(null);
+  if (!data) {
+    data = Object.create(null);
+  }
 
-  var read = function(filename, cached) {
+  var cache = new Map();
+
+  var read = function(filename) {
     var stat = fs.statSync(filename);
     if (stat.isDirectory()) {
       return createProxy(filename);
     }
+    var time = +stat.ctime;
+    var cached = cache.get(filename);
+    if (cached && cached.time >= time) {
+      return cached.data;
+    }
     var format = filename.split('.').pop();
-    return formats[format].sync(filename, format);
+    var data = formats[format].sync(filename, format);
+    cache.set(filename, {data: data, time: time});
+    return data;
   };
 
   var handler = {
     get: function(target, name) {
+
       // search for the directory, then the filenames
       var globs = [dir + '/' + name];
       globs.push(globs[0] + '.{' + formatExtensions + '}');
+
       // get all the files
       var files = globs.reduce(function(files, pattern) {
         return files.concat(glob.sync(pattern));
       }, []);
+
       // TODO: warn about ignored files here?
       var filename = files[0];
       if (filename) {
-        var cached = target[name];
-        return target[name] = read(filename, cached);
+        return read(filename);
       } else {
         return undefined;
       }
--- test/spec.js
+++ test/spec.js
@@ -1,8 +1,7 @@
 var assert = require('assert');
+var child = require('child_process');
 var datadir = require('../');
 var path = require('path');
-
-var createProxy = require('../proxy');
 
 var localDataPath = path.join(__dirname, '_data');
 
@@ -17,12 +16,49 @@ describe('load()', function() {
 
 });
 
-describe('proxy', function() {
+describe('proxy()', function() {
+
+  this.timeout(5000);
 
   it('creates proxies that read data', function() {
     var data = datadir.proxy(localDataPath);
     assert.deepEqual(data.test, {foo: 'bar'});
     assert.deepEqual(data.bar.x, {fizz: 'buzz', baz: 'quxx'});
+  });
+
+  xit('caches data', function() {
+    var data = datadir.proxy(localDataPath);
+    var test = data.test;
+    assert.strictEqual(data.test, test);
+  });
+
+  it('invalidates cached data', function(done) {
+    var data = datadir.proxy(localDataPath);
+
+    child.execSync([
+      'cp',
+      path.join(localDataPath, 'test.yml'),
+      path.join(localDataPath, 'invalidated.yml')
+    ].join(' '));
+
+    var test = data.invalidated;
+    assert.deepEqual(data.invalidated, {foo: 'bar'});
+
+    /**
+     * XXX we have to write the file a second later because some
+     * filesystems don't support sub-second precision.
+     */
+    setTimeout(function() {
+
+      child.execSync([
+        'echo', '"baz: qux"', '>>',
+        path.join(localDataPath, 'invalidated.yml')
+      ].join(' '));
+
+      assert.deepEqual(data.invalidated, {foo: 'bar', baz: 'qux'});
+      done();
+
+    }, 1001);
   });
 
 });
